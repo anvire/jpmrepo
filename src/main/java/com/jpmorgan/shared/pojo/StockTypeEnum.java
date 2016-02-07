39bf421aae54f72a3f6c1fa14bad3feab9e4b9e7
anvire <anvtcr@gmail.com>
2016-02-07 11:05:31 +0000

    Initial commit

---
+++ src/main/java/com/jpmorgan/shared/pojo/StockTypeEnum.java
@@ -0,0 +1,19 @@
+package com.jpmorgan.shared.pojo;
+
+/**
+ * Enumeration of stock types.
+ * @author AN
+ */
+public enum StockTypeEnum {
+
+    /*
+  	  * Common type.
+  	  */
+  	 COMMON,
+
+  	 /*
+  	  * Preferred type.
+  	  */
+  	 PREFERRED;
+
+}
